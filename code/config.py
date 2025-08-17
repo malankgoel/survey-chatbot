3144b1869ee80f8fc1c46a14f92a10a604b2f526
Malank Goel <goelmalank10@gmail.com>
2025-08-18 00:34:51 +0530

    Changes

--- code/config.py
+++ code/config.py
@@ -95,7 +95,7 @@ SYSTEM_PROMPT = f"""{Prompt}
 
 # API parameters
 MODEL = "gpt-4.1-2025-04-14"  
-MODEL_1 = "gpt-5-2025-08-07"
+MODEL_1 = "o3-2025-04-16"
 TEMPERATURE = None  # (None for default value)
 MAX_OUTPUT_TOKENS = 7000
 
